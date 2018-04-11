026df70baa10115622331a2c7f9308757a2fea80
Denise Kübler <denise@p058192.nat-gm.FH-Koeln.DE>
2018-04-11 13:56:41 +0200

    Aufgabe 1

---
+++ app.js
@@ -0,0 +1,15 @@
+
+var fs = require('fs');
+
+
+fs.readFile('/Users/denise/Downloads/Kickoff_Aufgabe/staedte.json','utf8', function (err, data) {
+  if (err)throw err;
+  console.log(data);
+
+    
+});
+                                                                                         
+                                                                                         
+                                                                                         
+                                                                                         
+

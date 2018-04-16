26cb24a9fdbdebaa0e47b8dc8a8e1a5d0005e0db
Kristian Czepluch <k.czepluch@yahoo.de>
2018-04-16 11:57:43 +0200

    Aufgabe 1 Richtige Version

--- Aufgabe 1/Kristian/app.js
+++ Aufgabe 1/Kristian/app.js
@@ -8,21 +8,10 @@ var fs = require("fs");
 fs.readFile('staedte.json','utf8', function (err, data) {
   if (err)throw err;
   var jsonContent = JSON.parse(data);
-
-for(var a=0;a<18;a++){
-    var s = JSON.stringify(jsonContent.cities[a]);
-    ausgabe(s);
-    console.log("\n----------------------\n");}
+  
+for(var a=0;a<jsonContent.cities.length;a++){
+    console.log(jsonContent.cities[a].name);
+    console.log(jsonContent.cities[a].country);
+    console.log(jsonContent.cities[a].population);
+    console.log(" --------------------------------- "); }    
 });
-
-
-// functions that erases '{'  ,  '}'  ','  and adds linebreaks after every '",'
-function ausgabe(s){
-   for(i=0;i<s.length;i++){
-    if(s[i]=="{" || s[i]=="}" ) { }
-    else if(s[i]+s[i-1]==("\",")) {
-        process.stdout.write("\n");}
-    else if(s[i]=="\"") { }
-    else process.stdout.write(s[i]);
-     } 
-}

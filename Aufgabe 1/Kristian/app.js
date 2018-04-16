72f62a0c00524e91fa642edb6c90d4f0f08481fd
Kristian Czepluch <k.czepluch@yahoo.de>
2018-04-16 12:11:17 +0200

    Aufgabe 2

--- Aufgabe 1/Kristian/app.js
+++ Aufgabe 1/Kristian/app.js
@@ -4,14 +4,15 @@ Reading out asynchornious data from a JSON file and printing it out formatted */
 
 
 var fs = require("fs");
+var chalk = require("chalk");
 
 fs.readFile('staedte.json','utf8', function (err, data) {
   if (err)throw err;
   var jsonContent = JSON.parse(data);
   
 for(var a=0;a<jsonContent.cities.length;a++){
-    console.log(jsonContent.cities[a].name);
-    console.log(jsonContent.cities[a].country);
-    console.log(jsonContent.cities[a].population);
+    console.log(chalk.blue(jsonContent.cities[a].name));
+    console.log(chalk.red(jsonContent.cities[a].country));
+    console.log(chalk.green(jsonContent.cities[a].population));
     console.log(" --------------------------------- "); }    
 });

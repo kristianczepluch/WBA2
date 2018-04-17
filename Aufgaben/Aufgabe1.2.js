eab182ec2bf18b7e3befef3f5bc759c9f017f7af
Kristian Czepluch <k.czepluch@yahoo.de>
2018-04-17 13:49:59 +0200

    Datein unbenannt

--- Aufgaben/Aufgabe1.2.js
+++ Aufgaben/Aufgabe1.2.js
@@ -8,16 +8,11 @@ var chalk = require("chalk");
 
 fs.readFile('staedte.json','utf8', function (err, data) {
   if (err)throw err;
-  var jsonContent = JSON.parse(data); 
-  jsonContent.cities.sort(function(a, b){return a.population - b.population});
-
-    fs.writeFile('staedte_sortiert.json', JSON.stringify(jsonContent),function (err){
-    
-    for(var a=0;a<jsonContent.cities.length;a++){
+  var jsonContent = JSON.parse(data);
+  
+for(var a=0;a<jsonContent.cities.length;a++){
     console.log(chalk.blue(jsonContent.cities[a].name));
     console.log(chalk.red(jsonContent.cities[a].country));
     console.log(chalk.green(jsonContent.cities[a].population));
-    console.log(" --------------------------------- "); } 
-
-  });});
-
+    console.log(" --------------------------------- "); }    
+});

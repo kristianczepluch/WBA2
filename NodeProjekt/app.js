3fdf9c24d41f5a41f6e91bccadef3b56626a03cf
Kristian Czepluch <k.czepluch@yahoo.de>
2018-06-18 11:49:26 +0200

    Beispiel Server mit Express hochgeladen

---
+++ NodeProjekt/app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res)=>{
+  res.send('Hallo Node.js');
+});
+
+
+const server = http.createServer(app);
+server.listen(3000, () => {
+  console.log('Server listening on Port 3000');
+});

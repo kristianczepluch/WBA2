9f2611e75eacb0fe1b24c9c0bb43ecbfda96a9b3
unknown <k.czepluch@yahoo.de>
2018-06-23 13:59:31 +0200

    Express example hinzugefügt

--- NodeProjekt/app.js
+++ NodeProjekt/app.js
@@ -1,12 +1,19 @@
+// define required modules
 const http = require('http');
 const express = require('express');
+
+// create express object
 const app = express();
+
+
+
 
 app.get('/', (req,res)=>{
   res.send('Hallo Node.js');
 });
 
 
+// create server which listens on port 3000
 const server = http.createServer(app);
 server.listen(3000, () => {
   console.log('Server listening on Port 3000');

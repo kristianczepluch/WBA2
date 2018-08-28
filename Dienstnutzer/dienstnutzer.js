b9c550d111f76a04cc777d07331295d513db283a
denisekuebler <denisekuebler97@gmail.com>
2018-08-28 19:50:11 +0200

    Parameter und 1 Request eingefügt

--- Dienstnutzer/dienstnutzer.js
+++ Dienstnutzer/dienstnutzer.js
@@ -1,88 +1,177 @@
-/* Dienstnutzer, welcher ein PubSub System implementiert, welches
-   Benutzern ermöglicht Tagesziele zu subscriben. Diese erhält der Benutzer
-   dann jeden Tag */
+/* Dienstnutzer, welcher als GUI fungiert und ein basic PubSub System enthält. */
 
-
+const request = require('request');
 const http = require('http');
 const faye = require('faye');
 const express = require('express');
 const menu = require('node-menu');
 var app = express();
+var dHost = 'https://wba2-heroku-tracker-service.herokuapp.com';
+
 
 menu.addDelimiter('*', 40, 'Health Tracker')
   .addItem(
     '\tBenutzer erstellen',
-    function() {
-      // Hier den Benutzer erstellen
-      // ...
-      console.log('Benutzer wurde erstellt!');
-    }).addItem(
+    function(username, usergender, userage, userheigt, userweight) {
+      // Code here
+
+    }, null, [{
+      'name': 'username',
+      'type': 'string'
+    }, {
+      'name': 'usergender',
+      'type': 'string'
+    }, {
+      'name': 'userage',
+      'type': 'numeric'
+    }, {
+      'name': 'userheigt',
+      'type': 'numeric'
+    }, {
+      'name': 'userweight',
+      'type': 'numeric'
+    }]).addItem(
     '\tBenutzer abfragen',
-    function() {
-      // Hier den Benutzer abfragen
-      // ...
-      console.log('Benutzer wurde abgefragt');
-    }).addItem(
+    function(benutzerId) {
+      var url = dHost + '/User/' + benutzerId;
+      request.get(url, function(err, res, body) {
+        body = JSON.parse(body);
+        console.log(body);
+      });
+
+    }, null, [{
+      'name': 'benutzerId',
+      'type': 'numeric'
+    }]).addItem(
     '\tBenutzer ändern',
-    function() {
-      // Hier den Benutzer ändern
-      // ...
-      console.log('Benutzer wurde geändert!');
-    }).addItem(
+    function(userId, username, usergender, userage, userheigt, userweight) {
+
+      // Code here
+
+    }, null, [{
+      'name': 'userId',
+      'type': 'numeric'
+    }, {
+      'name': 'username',
+      'type': 'string'
+    }, {
+      'name': 'usergender',
+      'type': 'string'
+    }, {
+      'name': 'userage',
+      'type': 'numeric'
+    }, {
+      'name': 'userheigt',
+      'type': 'numeric'
+    }, {
+      'name': 'userweight',
+      'type': 'numeric'
+    }]).addItem(
     '\tBenutzer löschen',
-    function() {
-      // Hier den Benutzer löschen
-      // ...
-      console.log('Benutzer wurde gelöscht!');
-    }).addItem(
+    function(userId) {
+
+      // Code here
+
+    }, null, [{
+      'name': 'benutzerId',
+      'type': 'numeric'
+    }]).addItem(
     '\tEintrag erstellen',
-    function() {
-      // Hier den Eintrag erstellen
-      // ...
-      console.log('Eintrag wurde erstellt.');
-    }).addItem(
+    function(userId, name, menge, kcal) {
+
+      // Code here
+
+    }, null, [{
+      'name': 'userId',
+      'type': 'numeric'
+    }, {
+      'name': 'name',
+      'type': 'string'
+    }, {
+      'name': 'menge',
+      'type': 'numeric'
+    }, {
+      'name': 'kcal',
+      'type': 'numeric'
+    }]).addItem(
     '\tEinträg abfragen',
-    function() {
-      // Hier die Einträge abfragen
-      // ...
-      console.log('Einträge wurden abgefragt.');
-    }).addItem(
-    '\tEintrag ändern',
-    function() {
-      // Hier den Eintrag verändern
-      // ...
-      console.log('Eintrag wurde verändert.');
-    }).addItem(
+    function(benutzerId, eintragId) {
+      // Code here
+    }, null, [{
+      'name': 'benutzerId',
+      'type': 'numeric'
+    }, {
+      'name': 'eintragId',
+      'type': 'numeric'
+    }]).addItem(
     '\tEintrag löschen',
-    function() {
-      // Hier den Eintrag löschen
-      // ...
-      console.log('Eintrag wurde gelöscht.');
-    }).addItem(
-    '\tMaximale Kcal abfragen',
-    function() {
-      // Hier das maximale an Kcal abfragen
-      // ...
-      console.log('Maximale Kcal wurden abgefragt.');
-    }).addItem(
+    function(userId, eintragId) {
+
+      // Code here
+
+    }, null, [{
+      'name': 'benutzerId',
+      'type': 'numeric'
+    }, {
+      'name': 'eintragId',
+      'type': 'numeric'
+    }]).addItem(
     '\tÜbrige Kcal abfragen',
-    function() {
-      // Hier die Übrigen Kcal ausgeben
-      // ...
-      console.log('Übrige Kcal wurden abgefragt.');
-    }).addItem(
+    function(benutzerId) {
+      // Code here
+
+    }, null, [{
+      'name': 'benutzerId',
+      'type': 'numeric'
+    }]).addItem(
     '\tMensa Speiseplan abfragen',
-    function() {
-      // Hier den Speiseplan abfragen
-      // ...
-      console.log('Der Speiseplan wurde abgefragt.');
-    }).addItem(
+    function(monat, tag) {
+
+      var url = dHost + '/MensaGm/Speiseplan/' + monat + '/' + tag;
+      request.get(url, function(err, res, body) {
+        body = JSON.parse(body);
+        console.log(body);
+      });
+
+    }, null, [{
+      'name': 'monat',
+      'type': 'numeric'
+    }, {
+      'name': 'tag',
+      'type': 'string'
+    }]).addItem(
     '\tMensagericht eintragen',
-    function() {
-      // Hier ein Mensagericht eintragen
-      // ...
-      console.log('Der Speiseplan wurde abgefragt.');
-    }).addItem(
+    function(gericht, userId) {
+
+      var url = dHost + '/User/MensaGm/Gerichte/' + gericht + '/' + userId;
+
+      var options = {
+        uri: url,
+        method: 'POST'
+      }
+
+      request(options, function(err, res, body) {
+        console.log(body);
+      });
+
+    }, null, [{
+      'name': 'gericht',
+      'type': 'string'
+    }, {
+      'name': 'userId',
+      'type': 'numeric'
+    }]).addItem(
+    '\tMensabeilage eintragen',
+    function(beilage, userId) {
+      // Code here
+
+    }, null, [{
+      'name': 'beilage',
+      'type': 'string'
+    }, {
+      'name': 'userId',
+      'type': 'numeric'
+    }]).addItem(
     '\tTopic abonnieren',
     function(topic) {
       client.subscribe(topic, function(message) {

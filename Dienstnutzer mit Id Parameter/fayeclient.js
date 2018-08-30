e587ca15bdbfb6718d6ebb83c8d30dac47ff8cc3
Kristian Czepluch <k.czepluch@yahoo.de>
2018-08-30 19:52:40 +0200

    Für die Abgabe nicht relevantes Beispiel entfernt

--- Dienstnutzer mit Id Parameter/fayeclient.js
+++
@@ -1,8 +0,0 @@
-var faye = require('faye');
-const http = require('http');
-const readline = require('readline');
-
-var client = new faye.Client('http://localhost:3000/faye');
-client.subscribe('/vitamins', function(message){
-  console.log(message.text);
-});
